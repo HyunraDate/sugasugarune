770c5eccef2c44ad5af5a3a7e6cfe37967bf2ba8
Hyunra <118969011+HyunraDate@users.noreply.github.com>
2023-02-01 15:21:10 +0100

    Add files via upload

---
+++ Frame_2.js
@@ -0,0 +1,15 @@
+function onClick() {
+    window.location.href =      'Step2_A.html';
+  }
+  
+  function onClick_1() {
+    location.href = '../Step2_B/Step2_B.html';
+  }
+
+  function onClick_2() {
+    location.href = '../Step2_C/Step2_C.html';
+  }
+  
+  function onClick_3() {
+    location.href = '../Step2_D/Step2_D.html';
+  }
